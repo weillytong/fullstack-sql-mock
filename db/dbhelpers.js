d740d48f40e58ed362601fc88fc0496bf56b5904
weillytong <weillydtong@gmail.com>
2021-02-21 14:40:30 -0800

    Completed backend implementation

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -1,11 +1,35 @@
 // complete and fix the dbhelpers
+const db = require('./index.js');
 
 
-getProductsHelper = () => 
+var dbhelpers = {
+  getProductsHelper: (callback) => {
+    var queryString = `SELECT * FROM products`
+    db.query(queryString, (err, results) => {
+      callback(err, results);
+    })
+  },
 
-postProductsHelper = () => 
+  postProductsHelper: (req, callback) => {
+    var queryString = `INSERT INTO products (item, min_cost, curr_bid, ends_in, image) VALUES ('${req.body.item}', '${req.body.min_cost}', ${req.body.curr_bid}, ${req.body.ends_in}, '${req.body.image}')`
+    db.query(queryString, (err, results) => {
+      callback(err, results);
+    })
+  },
 
-updateProductHelper = () => 
+  updateProductHelper: (req, callback) => {
+    var queryString = `UPDATE products SET curr_bid=${req.body.curr_bid} WHERE id=${req.params._id}`
+    db.query(queryString, (err, results) => {
+      callback(err, results);
+    })
+  },
 
-deleteProductHelper = () => 
+  deleteProductHelper: (req, callback) => {
+    var queryString = `DELETE FROM products WHERE id=${req.params._id}`
+    db.query(queryString, (err, results) => {
+      callback(err, results);
+    })
+  }
+}
 
+module.exports = dbhelpers;
