0dcd0ae41597b6d9f7dd804371fbe50610b583b3
weillytong <weillydtong@gmail.com>
2021-02-21 12:56:28 -0800

    Set up database schema and connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,17 @@
+DROP DATABASE IF EXISTS ebayDB;
+
+CREATE DATABASE ebayDB;
+
+USE ebayDB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  item VARCHAR(255),
+  min_cost FLOAT,
+  current_bid FLOAT,
+  ends_in INT,
+  img VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
 
